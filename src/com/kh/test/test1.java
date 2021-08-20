f0405c033d4ad2bfa736b651174117074cb79c0f
Meesou <huny0529@gmail.com>
2021-08-20 21:37:53 +0900

    올라가나요..?

--- src/com/kh/test/test1.java
+++ src/com/kh/test/test1.java
@@ -4,6 +4,7 @@ public class test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	테스트하
 
 	}
 
---
+++ src/com/kh/test/test22.java
@@ -0,0 +1,11 @@
+package com.kh.test;
+
+public class test22 {
+	
+	public test22() {
+		// TODO Auto-generated constructor stub
+		
+		System.out.println("왜..");
+	}
+
+}
